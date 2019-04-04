02a5d6bc2a56edd2d0b1ecd0d0d76e6a2024a4e1
Rishit Ratna <rishitratan5@gmail.com>
2019-04-04 18:59:12 +0530

    final commit

---
+++ first.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+protected class Factorial {
+
+    public static void main(String[] args) {
+	
+	int num = 10;
+	int i
+        for(i = 1; i <= num; ++i)
+        {
+            
+            factorial *= i;
+        }
+	Scanner in = new Scanner(System.in);
+        System.out.print("Factorial of %d = %d", num, factorial);
+    }
+}
